387ac7543e7f228bc5f2aadccafa3435ae456f2a
Karol158 <159441394+Karol158@users.noreply.github.com>
2024-11-25 14:29:40 -0300

    Livros.java

--- src/biblioteca/Livros.java
+++ src/biblioteca/Livros.java
@@ -5,11 +5,11 @@ public abstract class Livros {
     private String capa;
     private String nome_livro;
     private String nome_autor;
-    private String ano_escrito;
+    private int ano_escrito;
     private String genero;
-    private String ano_publicado;
+    private int ano_publicado;
     
-    Livros(String capa,String nome_livro,String nome_autor,String ano_escrito,String genero,String ano_publicado){
+    Livros(String capa,String nome_livro,String nome_autor,int ano_escrito,String genero,int ano_publicado){
     this.capa=capa;
     this.nome_livro=nome_livro;
     this.nome_autor=nome_autor;
@@ -26,16 +26,16 @@ public abstract class Livros {
     public abstract String getNomeautor();
     public abstract void setNomeautor(String nome_autor);
       
-    public abstract String getAnoescrito();
-    public abstract void setAnoescrito(String ano_escrito);
+    public abstract int getAnoescrito();
+    public abstract void setAnoescrito(int ano_escrito);
        
     public abstract String getGenero();
     
     public abstract void setGenero(String genero);
         
-    public abstract String getAnopublicado();
+    public abstract int getAnopublicado();
        
-    public abstract void setAnopublicado(String ano_publicado);
+    public abstract void setAnopublicado(int ano_publicado);
     
     
 }
