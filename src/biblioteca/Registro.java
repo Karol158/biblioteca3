476b9ab86e3437b238d2f882c5b7c3e467bdc4f4
Karol158 <159441394+Karol158@users.noreply.github.com>
2024-11-29 10:12:06 -0300

    Update Registro.java

--- src/biblioteca/Registro.java
+++ src/biblioteca/Registro.java
@@ -7,93 +7,94 @@ public class Registro extends Livros {
      
      
      Registro(String capa, String nome_livro, String nome_autor, int ano_escrito, 
-     String genero, int ano_publicado,String data_pegoulivro,String data_devolucao,String nome_leitor){
-            super(capa,nome_livro,nome_autor,ano_escrito,genero,ano_publicado);
-               this.data_pegoulivro=data_pegoulivro;
-               this.data_devolucao=data_devolucao;
-               this.nome_leitor=nome_leitor;
+     String genero, int ano_publicado, String data_pegoulivro, String data_devolucao, String nome_leitor) {
+         super(capa, nome_livro, nome_autor, ano_escrito, genero, ano_publicado); 
+         this.data_pegoulivro = data_pegoulivro;
+         this.data_devolucao = data_devolucao;
+         this.nome_leitor = nome_leitor;
      }
-     public String getDatapegoulivro(){
-        return this.data_pegoulivro;
-    }
-    
-    public void setDatapegoulivro(String data_pegoulivro){
-        this.data_pegoulivro=data_pegoulivro;
-    
-    }
-    public String getDatadevolucao(){
-        return this.data_devolucao;
-    }
-    
-    public void setDatadevolucao(String data_devolucao){
-        this.data_devolucao=data_devolucao;
-    
-    }
-    public String getNomeLeitor(){
-        return this.nome_leitor;
-    }
-    
-    public void setNomeLeitor(String nome_leitor){
-        this.nome_leitor=nome_leitor;
-    
-    }
-@Override
+     
+     public String getDataPegouLivro() {
+         return this.data_pegoulivro;
+     }
+ 
+     public void setDataPegouLivro(String data_pegoulivro) {
+         this.data_pegoulivro = data_pegoulivro;
+     }
+ 
+     public String getDataDevolucao() {
+         return this.data_devolucao;
+     }
+ 
+     public void setDataDevolucao(String data_devolucao) {
+         this.data_devolucao = data_devolucao;
+     }
+ 
+     public String getNomeLeitor() {
+         return this.nome_leitor;
+     }
+ 
+     public void setNomeLeitor(String nome_leitor) {
+         this.nome_leitor = nome_leitor;
+     }
+
+    @Override
     public String getCapa() {
-        return super.getCapa();
+        return getCapa();
     }
 
     @Override
     public void setCapa(String capa) {
-        super.setCapa(capa);
+        setCapa(capa);
     }
 
     @Override
     public String getNomelivro() {
-        return super.getNomelivro();
+        return getNomelivro();
     }
 
     @Override
     public void setNomelivro(String nome_livro) {
-        super.setNomelivro(nome_livro);
+        setNomelivro(nome_livro);
     }
 
     @Override
     public String getNomeautor() {
-        return super.getNomeautor();
+        return getNomeautor();
     }
 
     @Override
     public void setNomeautor(String nome_autor) {
-        super.setNomeautor(nome_autor);
+        setNomeautor(nome_autor);
     }
 
     @Override
     public int getAnoescrito() {
-        return super.getAnoescrito();
+        return getAnoescrito();
     }
 
     @Override
     public void setAnoescrito(int ano_escrito) {
-        super.setAnoescrito(ano_escrito);
+        setAnoescrito(ano_escrito);
     }
 
     @Override
     public String getGenero() {
-        return super.getGenero();
+        return getGenero();
     }
 
     @Override
     public void setGenero(String genero) {
-        super.setGenero(genero);
+        setGenero(genero);
     }
 
     @Override
     public int getAnopublicado() {
-        return super.getAnopublicado();
+        return getAnopublicado();
     }
 
     @Override
     public void setAnopublicado(int ano_publicado) {
-        super.setAnopublicado(ano_publicado);
+       setAnopublicado(ano_publicado);
     }
 }
